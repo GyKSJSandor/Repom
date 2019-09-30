b38300eeeefee124429a7fb5ae9aba290575d7ca
GyKSJSandor <46829744+GyKSJSandor@users.noreply.github.com>
2019-09-30 21:49:00 +0200

    Add files via upload

---
+++ prospring/src/main/java/com/gyksj/prospring/ProspringApplication.java
@@ -0,0 +1,13 @@
+package com.gyksj.prospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspringApplication.class, args);
+	}
+
+}
---
+++ prospring/src/test/java/com/gyksj/prospring/ProspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gyksj.prospring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
